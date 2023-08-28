fa1bd925a18e928f5ca7601121761d8387dfb131
Natalya Anatskaya <taff2011@yandex.ru>
2023-08-28 15:42:34 +0300

    Сверстано оба разрешения

---
+++ js/main.js
@@ -0,0 +1,11 @@
+new Swiper('.swiper-container', {
+	grabCursor: true,
+	/*loop: true,*/
+
+	spaceBetween: 25,
+
+	navigation: {
+		nextEl: '.event__arrow_right',
+		prevEl: '.event__arrow_left',
+	},
+})
